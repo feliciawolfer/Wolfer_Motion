491ace0e1db1416d0f3a0c6483f3c04c156f7cb0
FeliciaJW <102631983+FeliciaJW@users.noreply.github.com>
2022-10-18 09:36:36 -0400

    content text changed

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,5 +1,24 @@
 import $ from "jquery";
-// import gsap from "gsap";
+import gsap from "gsap";
+
+gsap.to("#gallery-1", {duration:0, alpha: 0});
+gsap.to("#gallery-1", {duration:1, alpha: 1});
+
+gsap.to("#gallery-2", {duration:0, alpha: 0});
+gsap.to("#gallery-2", {duration:1, alpha: 1});
+
+gsap.to("#gallery-3", {duration:0, alpha: 0});
+gsap.to("#gallery-3", {duration:1, alpha: 1});
+
+gsap.to("#gallery-4", {duration:0, alpha: 0});
+gsap.to("#gallery-4", {duration:1, alpha: 1});
+
+gsap.to("#gallery-5", {duration:0, alpha: 0});
+gsap.to("#gallery-5", {duration:1, alpha: 1});
+
+gsap.to("#gallery-6", {duration:0, alpha: 0});
+gsap.to("#gallery-6", {duration:1, alpha: 1});
+
 
 let $nav = $('nav[data-nav]');
 let isVisible = false;
