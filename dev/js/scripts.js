7b92091623869eda9ee643d4b235ab99017a3156
FeliciaJW <102631983+FeliciaJW@users.noreply.github.com>
2022-10-20 01:24:45 -0400

    hero-2 animation

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -3,6 +3,51 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
+gsap.to("#hero-2", {
+    duration: 5,
+    clipPath: "inset(50% 0)",
+    scrollTrigger: {
+        trigger: "#hero-2",
+        markers: true,
+        scrub: true,
+        end: "bottom center",
+        start: "top 10%"
+    }
+})
+gsap.to("#hero-2 h1", {
+    duration: 5,
+    scale:3,
+    alpha:0,
+    clipPath: "inset(50% 0)",
+    scrollTrigger: {
+        trigger: "#hero-2",
+        markers: true,
+        scrub: true,
+        end: "bottom center",
+        start: "top 10%"
+    }
+});
+gsap.to("#hero-2", {
+    duration: 5,
+    scale:2,
+    alpha:0,
+    clipPath: "inset(50% 0)",
+    scrollTrigger: {
+        trigger: "#hero-2",
+        markers: true,
+        scrub: true,
+        end: "bottom center",
+        start: "top 10%"
+    }
+});
+
+
+// function hero2Animation(){
+//     var tl =gsap.timeline ();
+//     tl.from("bg-img",{duration:1, scaleX:0})
+//     return tl;
+// }
+
 gsap.to("#gallery-1", {
     duration: 0,
     alpha: 0
@@ -10,7 +55,13 @@ gsap.to("#gallery-1", {
 gsap.to("#gallery-1", {
     duration: 1,
     alpha: 1,
-    scrollTrigger: { trigger: "#gallery-1", markers: true, scrub:true, end: "top center", start:"top 80%"}
+    scrollTrigger: {
+        trigger: "#gallery-1",
+        markers: true,
+        scrub: true,
+        end: "top center",
+        start: "top 80%"
+    }
 });
 
 gsap.to("#gallery-2", {
@@ -20,7 +71,13 @@ gsap.to("#gallery-2", {
 gsap.to("#gallery-2", {
     duration: 1,
     alpha: 1,
-    scrollTrigger: { trigger: "#gallery-2", markers: true, scrub:true, end: "top center", start:"top 80%"}
+    scrollTrigger: {
+        trigger: "#gallery-2",
+        markers: true,
+        scrub: true,
+        end: "top center",
+        start: "top 80%"
+    }
 });
 
 gsap.to("#gallery-3", {
@@ -30,7 +87,13 @@ gsap.to("#gallery-3", {
 gsap.to("#gallery-3", {
     duration: 2,
     alpha: 1,
-    scrollTrigger: { trigger: "#gallery-3", markers: true, scrub:true, end: "top center", start:"top 80%"}
+    scrollTrigger: {
+        trigger: "#gallery-3",
+        markers: true,
+        scrub: true,
+        end: "top center",
+        start: "top 80%"
+    }
 });
 
 gsap.to("#gallery-4", {
@@ -40,7 +103,13 @@ gsap.to("#gallery-4", {
 gsap.to("#gallery-4", {
     duration: 1,
     alpha: 1,
-    scrollTrigger: { trigger: "#gallery-4", markers: true, scrub:true, end: "top center", start:"top 80%"}
+    scrollTrigger: {
+        trigger: "#gallery-4",
+        markers: true,
+        scrub: true,
+        end: "top center",
+        start: "top 80%"
+    }
 });
 
 gsap.to("#gallery-5", {
@@ -50,7 +119,13 @@ gsap.to("#gallery-5", {
 gsap.to("#gallery-5", {
     duration: 1,
     alpha: 1,
-    scrollTrigger: { trigger: "#gallery-5", markers: true, scrub:true, end: "top center", start:"top 80%"}
+    scrollTrigger: {
+        trigger: "#gallery-5",
+        markers: true,
+        scrub: true,
+        end: "top center",
+        start: "top 80%"
+    }
 });
 
 gsap.to("#gallery-6", {
@@ -60,7 +135,13 @@ gsap.to("#gallery-6", {
 gsap.to("#gallery-6", {
     duration: 1,
     alpha: 1,
-    scrollTrigger: { trigger: "#gallery-6", markers: true, scrub:true, end: "top center", start:"top 80%"}
+    scrollTrigger: {
+        trigger: "#gallery-6",
+        markers: true,
+        scrub: true,
+        end: "top center",
+        start: "top 80%"
+    }
 });
 
 gsap.to("#gallery-7", {
@@ -70,30 +151,14 @@ gsap.to("#gallery-7", {
 gsap.to("#gallery-7", {
     duration: 1,
     alpha: 1,
-    scrollTrigger: { trigger: "#gallery-7", markers: true, scrub:true, end: "top center", start:"top 80%"}
+    scrollTrigger: {
+        trigger: "#gallery-7",
+        markers: true,
+        scrub: true,
+        end: "top center",
+        start: "top 80%"
+    }
 });
-
-// var galleryTl = gsap.timeline();
-
-
-// galleryTl.to("#gallery-1", {duration:0, alpha: 0})
-//     .to("#gallery-1", {duration:1, alpha: 1})
-
-//     .to("#gallery-2", {duration:0, alpha: 0})
-//     .to("#gallery-2", {duration:1, alpha: 1},"-=1")
-
-//     .to("#gallery-3", {duration:0, alpha: 0})
-//     .to("#gallery-3", {duration:2, alpha: 1},"-=1")
-
-//     .to("#gallery-4", {duration:0, alpha: 0})
-//     .to("#gallery-4", {duration:1, alpha: 1},"-=1")
-
-//     .to("#gallery-5", {duration:0, alpha: 0})
-//     .to("#gallery-5", {duration:1, alpha: 1},"-=1")
-
-//     .to("#gallery-6", {duration:0, alpha: 0})
-//     .to("#gallery-6", {duration:1, alpha: 1},"-=1");
-
 
 let $nav = $('nav[data-nav]');
 let isVisible = false;
