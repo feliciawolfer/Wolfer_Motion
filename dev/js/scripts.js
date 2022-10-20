1d8fdb5e5661b5ae29f0ac431ac07a6ace25e8cb
FeliciaJW <102631983+FeliciaJW@users.noreply.github.com>
2022-10-20 11:23:29 -0400

    Hero-2 and gallery timelines added

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -42,174 +42,30 @@ gsap.from("#subhead", {
     }
 });
 
-// gsap.from("#history-middle", {
+function hero2Motion(){
+    var tl = gsap.timeline({scrollTrigger:{trigger:"#hero-2",scrub:true, markers:false, start:"top 60%", end:"bottom 80%"}});
+    tl.from("#hero-2", {duration: 2, alpha:0,clipPath: "inset(50% 0)"})
+    .from("#hero-2 h1", {duration: 2, scale:3,alpha:0},"-=1")
+    .from("#hero-2 h2", {duration: 2, scale:2,alpha:0},"-=1");
+    return tl;
+}
 
-//     scrollTrigger: {
-//         trigger: "#history-middle",
-//         markers: true,
-//         scrub: true,
-//         end: "center, 70%",
-//         start: "80%, 99%"
-//     }
-// });
-
-gsap.to("#hero-2", {
-    duration: 5,
-    alpha:0,
-    clipPath: "inset(50% 0)",
-    scrollTrigger: {
-        trigger: "#hero-2",
-        // markers: true,
-        scrub: true,
-        end: "bottom center",
-        start: "top 10%"
-    }
-});
-gsap.to("#hero-2 h1", {
-    duration: 5,
-    scale:3,
-    alpha:1,
-    // clipPath: "inset(50% 0)",
-    scrollTrigger: {
-        trigger: "#hero-2",
-        // markers: true,
-        scrub: true,
-        end: "bottom center",
-        start: "top 10%"
-    }
-});
-gsap.to("#hero-2 h2", {
-    duration: 5,
-    scale:2,
-    alpha:1,
-    // clipPath: "inset(50% 0)",
-    scrollTrigger: {
-        trigger: "#hero-2",
-        // markers: true,
-        scrub: true,
-        end: "bottom center",
-        start: "top 10%"
-    }
-});
+function galleryMotion(){
+     var tl= gsap.timeline({scrollTrigger:{trigger:"#gallery-7",scrub:true, markers:true, end: "top center", start: "top 80%"}});
+      tl.from("#gallery-1", {duration: 2, alpha: 0 })
+        .from("#gallery-5", {duration: 2, alpha: 0 })
+        .from("#gallery-2", {duration: 2, alpha: 0 })
+        .from("#gallery-4", {duration: 2, alpha: 0 })
+        .from("#gallery-3", {duration: 2, alpha: 0 })
+        .from("#gallery-6", {duration: 2, alpha: 0 })
+        .from("#gallery-7", {duration: 2, alpha: 0 });
+        return tl
+}
 
 
-// function hero2Animation(){
-//     var tl =gsap.timeline ();
-//     tl.from("bg-img",{duration:1, scaleX:0})
-//     return tl;
-// }
-
-gsap.to("#gallery-1", {
-    duration: 0,
-    alpha: 0
-});
-gsap.to("#gallery-1", {
-    duration: 1,
-    alpha: 1,
-    scrollTrigger: {
-        trigger: "#gallery-1",
-        // markers: true,
-        scrub: true,
-        end: "top center",
-        start: "top 80%"
-    }
-});
-
-gsap.to("#gallery-2", {
-    duration: 0,
-    alpha: 0
-});
-gsap.to("#gallery-2", {
-    duration: 1,
-    alpha: 1,
-    scrollTrigger: {
-        trigger: "#gallery-2",
-        // markers: true,
-        scrub: true,
-        end: "top center",
-        start: "top 80%"
-    }
-});
-
-gsap.to("#gallery-3", {
-    duration: 0,
-    alpha: 0
-});
-gsap.to("#gallery-3", {
-    duration: 2,
-    alpha: 1,
-    scrollTrigger: {
-        trigger: "#gallery-3",
-        // markers: true,
-        scrub: true,
-        end: "top center",
-        start: "top 80%"
-    }
-});
-
-gsap.to("#gallery-4", {
-    duration: 0,
-    alpha: 0
-});
-gsap.to("#gallery-4", {
-    duration: 1,
-    alpha: 1,
-    scrollTrigger: {
-        trigger: "#gallery-4",
-        // markers: true,
-        scrub: true,
-        end: "top center",
-        start: "top 80%"
-    }
-});
-
-gsap.to("#gallery-5", {
-    duration: 0,
-    alpha: 0
-});
-gsap.to("#gallery-5", {
-    duration: 1,
-    alpha: 1,
-    scrollTrigger: {
-        trigger: "#gallery-5",
-        // markers: true,
-        scrub: true,
-        end: "top center",
-        start: "top 80%"
-    }
-});
-
-gsap.to("#gallery-6", {
-    duration: 0,
-    alpha: 0
-});
-gsap.to("#gallery-6", {
-    duration: 1,
-    alpha: 1,
-    scrollTrigger: {
-        trigger: "#gallery-6",
-        // markers: true,
-        scrub: true,
-        end: "top center",
-        start: "top 80%"
-    }
-});
-
-gsap.to("#gallery-7", {
-    duration: 0,
-    alpha: 0
-});
-gsap.to("#gallery-7", {
-    duration: 1,
-    alpha: 1,
-    scrollTrigger: {
-        trigger: "#gallery-7",
-        // markers: true,
-        scrub: true,
-        end: "top center",
-        start: "top 80%"
-    }
-});
+var mainTl = gsap.timeline();
+mainTl.add(hero2Motion())
+.add(galleryMotion());
 
 let $nav = $('nav[data-nav]');
 let isVisible = false;
