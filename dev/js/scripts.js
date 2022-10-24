bd26981d9fc07208f88210138a62adc0ea6bac78
FeliciaJW <102631983+FeliciaJW@users.noreply.github.com>
2022-10-24 19:22:11 -0400

    Revert "hero animation, background color gradient"
    This reverts commit d555d19e56d4b62df2521bfc9a2179031f83a49d.

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -42,36 +42,25 @@ gsap.registerPlugin(ScrollTrigger);
 //     }
 // });
 
-function headerMotion(){
-    var tl = gsap.timeline({scrollTrigger:{trigger:"#people", markers:false, scrub:true, end: "bottom 70%", start: "bottom 90%"}})
+function heroMotion(){
+    var tl = gsap.timeline({scrollTrigger:{trigger:"#spaces", markers:false, scrub:true, end: "bottom, top 50%", start: "top, center"}})
         tl.from("#spaces", { duration:1, alpha:0, x:-200})
-        .from("#people", { duration:1,scale:2.5, y:-55}, "-=0.75")
-        .from("#subhead", {duration:1, alpha:0, y:-40},"-=1")
+        .from("#people", { duration:1,scale:2.5})
+        .from("#subhead", {duration:1, alpha:0, scale:-2, y:-50})
     return tl;
 }
 
-function historyMotion(){
-    var tl = gsap.timeline({scrollTrigger:{trigger:"#history-middle", markers:true, scrub:true, end: "bottom 70%", start: "bottom bottom"}})
-       tl.from("#history-middle", {duration:1, alpha:0, x:80},"-=1")
-        .from("#header-1", {duration:1, alpha:0, x:80},"-=2")
-        .from("#paragraph-1", {duration:1, alpha:0, x:80},"-=3")
-        .from("#header-2", {duration:1, alpha:0, x:80},"-=4")
-        .from("#paragraph-2", {duration:1, alpha:0, x:80},"-=5")
-        .from("#header-3", {duration:1, alpha:0, x:80},"-=6")
-        .from("#paragrph-3", {duration:1, alpha:0, x:80},"-=7")
-    return tl;
-}
 
 function hero2Motion(){
-    var tl = gsap.timeline({scrollTrigger:{trigger:"#hero-2",scrub:true, markers:false, start:"10% bottom", end:"center center"}});
-     tl.to("#hero-2", {duration: 2, alpha:1, clipPath: "circle(100%)"})
-       .from("#hero-2 h1", {duration: 2, scale:3, alpha:0},"-=1")
-       .from("#hero-2 h2", {duration: 2, scale:2, alpha:0},"-=1");
+    var tl = gsap.timeline({scrollTrigger:{trigger:"#hero-2",scrub:true, markers:false, start:"top 60%", end:"bottom 80%"}});
+    tl.from("#hero-2", {duration: 2, alpha:0, clipPath: "inset(50% 0)"})
+    .from("#hero-2 h1", {duration: 2, scale:3, alpha:0},"-=1")
+    .from("#hero-2 h2", {duration: 2, scale:2, alpha:0},"-=1");
     return tl;
 }
 
 function galleryMotion(){
-    var tl= gsap.timeline({scrollTrigger:{trigger:"#gallery-1", scrub:true, markers:false, end: "center 20%", start: "center 90%"}});
+    var tl= gsap.timeline({scrollTrigger:{trigger:"#gallery-1", scrub:true, markers:true, end: "center 20%", start: "center 90%"}});
      tl.from("#gallery-1", {duration: 3, clipPath: "circle(100%)",alpha: 0 })
        .from("#gallery-5", {duration: 3, clipPath: "circle(100%)",alpha: 0 },"-=1.5")
        .from("#gallery-2", {duration: 3, clipPath: "circle(100%)",alpha: 0 },"-=1.5")
@@ -96,8 +85,7 @@ function galleryMotion(){
 
 
 var mainTl = gsap.timeline();
-mainTl.add(headerMotion())
-.add(historyMotion())
+mainTl.add(heroMotion())
 .add(hero2Motion())
 .add(galleryMotion());
 
