6688bb8b9707ed63269fe752cf7f7374355e6d72
FeliciaJW <102631983+FeliciaJW@users.noreply.github.com>
2022-10-24 21:14:09 -0400

    hero-2 animation fixed, header animation complete

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -3,20 +3,46 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
+// const showAnim = gsap.from('.main-tool-bar', { 
+//     yPercent: -100,
+//     paused: true,
+//     duration: 0.2
+//   }).progress(1);
+  
+//   ScrollTrigger.create({
+//     start: "top top",
+//     end: 99999,
+//     onUpdate: (self) => {
+//       self.direction === -1 ? showAnim.play() : showAnim.reverse()
+//     }
+//   });
+
 function heroMotion(){
     var tl = gsap.timeline({scrollTrigger:{trigger:"#spaces", markers:false, scrub:true, end: "bottom 45%", start: "top 60%"}})
         tl.from("#spaces", { duration:1, alpha:0, x:-200})
         .from("#people", { duration:1,scale:2.5},"-=1")
         .from("#subhead", {duration:1, alpha:0, y:-50},"-=1")
+        .from("#history-middle", {duration:1, alpha:0, y:-50},)
+    return tl;
+}
+
+function historytextMotion(){
+    var tl = gsap.timeline({scrollTrigger:{trigger:"#header-1", markers:false, scrub:true, end: "bottom 45%", start: "top 80%"}})
+        tl.from("#header-1", { duration:1, alpha:0, x:-200})
+        .from("#paragraph-1", { duration:1, alpha:0, x:200})
+        .from("#header-2", { duration:1, alpha:0, x:-200})
+        .from("#paragraph-2", { duration:1, alpha:0, x:200})
+        .from("#header-3", { duration:1, alpha:0, x:-200})
+        .from("#paragraph-3", { duration:1, alpha:0, x:200})
     return tl;
 }
 
 
 function hero2Motion(){
-    var tl = gsap.timeline({scrollTrigger:{trigger:"#hero-2",scrub:true, markers:false, start:"top 60%", end:"bottom 80%"}});
+    var tl = gsap.timeline({scrollTrigger:{trigger:"#hero-2",scrub:true, markers:false, start:"top bottom", end:"center 60%"}});
     tl.from("#hero-2", {duration: 2, alpha:0, clipPath: "inset(50% 0)"})
-    .from("#hero-2 h1", {duration: 2, scale:3, alpha:0},"-=1")
-    .from("#hero-2 h2", {duration: 2, scale:2, alpha:0},"-=1");
+    .from("#hero-2 h1", {duration: 2, scale:10, color:"pink"},"-=.85")
+    .from("#hero-2 h2", {duration: 2, scale:10, color:"pink"},"-=.95");
     return tl;
 }
 
@@ -47,6 +73,7 @@ function galleryMotion(){
 
 var mainTl = gsap.timeline();
 mainTl.add(heroMotion())
+.add(historytextMotion())
 .add(hero2Motion())
 .add(galleryMotion());
 
