2b43a25fdfb8c3c4b235b5962e6c93eb1db704d4
FeliciaJW <102631983+FeliciaJW@users.noreply.github.com>
2022-10-24 23:45:32 -0400

    gallery animation complete

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -3,26 +3,12 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-// const showAnim = gsap.from('.main-tool-bar', { 
-//     yPercent: -100,
-//     paused: true,
-//     duration: 0.2
-//   }).progress(1);
-  
-//   ScrollTrigger.create({
-//     start: "top top",
-//     end: 99999,
-//     onUpdate: (self) => {
-//       self.direction === -1 ? showAnim.play() : showAnim.reverse()
-//     }
-//   });
-
 function heroMotion(){
     var tl = gsap.timeline({scrollTrigger:{trigger:"#spaces", markers:false, scrub:true, end: "bottom 45%", start: "top 60%"}})
         tl.from("#spaces", { duration:1, alpha:0, x:-200})
-        .from("#people", { duration:1,scale:2.5},"-=1")
+        .from("#people", { duration:1,scale:2.5, color: "black"},"-=1")
         .from("#subhead", {duration:1, alpha:0, y:-50},"-=1")
-        .from("#history-middle", {duration:1, alpha:0, y:-50},)
+        .from("#history-middle", {duration:1, alpha:0, y:-50})
     return tl;
 }
 
@@ -37,7 +23,6 @@ function historytextMotion(){
     return tl;
 }
 
-
 function hero2Motion(){
     var tl = gsap.timeline({scrollTrigger:{trigger:"#hero-2",scrub:true, markers:false, start:"top bottom", end:"center 60%"}});
     tl.from("#hero-2", {duration: 2, alpha:0, clipPath: "inset(50% 0)"})
@@ -47,26 +32,38 @@ function hero2Motion(){
 }
 
 function galleryMotion(){
-    var tl= gsap.timeline({scrollTrigger:{trigger:"#gallery-1", scrub:true, markers:false, end: "center 20%", start: "center 90%"}});
-     tl.from("#gallery-1", {duration: 3, clipPath: "circle(100%)",alpha: 0 })
-       .from("#gallery-5", {duration: 3, clipPath: "circle(100%)",alpha: 0 },"-=1.5")
-       .from("#gallery-2", {duration: 3, clipPath: "circle(100%)",alpha: 0 },"-=1.5")
-       .from("#gallery-4", {duration: 3, clipPath: "circle(100%)",alpha: 0 },"-=1.5")
-       .from("#gallery-3", {duration: 3, clipPath: "circle(100%)",alpha: 0 },"-=1.5")
-       .from("#gallery-6", {duration: 3, clipPath: "circle(100%)",alpha: 0 })
-       .from("#gallery-7", {duration: 3, alpha: 0 }, "+=8");
+    var tl= gsap.timeline({scrollTrigger:{trigger:"#gallery-1", scrub:true, markers:true, end: "center center", start: "center bottom"}});
+     tl.from("#gallery-1", {duration: 3, clipPath: "inset(100%)",alpha: 0})
+       .from("#gallery-5", {duration: 3,clipPath: "inset(100%)",alpha: 0 },"-=1.5")
+       .from("#gallery-2", {duration: 3,clipPath: "inset(100%)",alpha: 0 },"-=1.5")
+       .from("#gallery-4", {duration: 3,clipPath: "inset(100%)",alpha: 0 },"-=1.5")
+       .from("#gallery-3", {duration: 3,clipPath: "inset(100%)",alpha: 0 },)
+       .from("#gallery-6", {duration: 3,clipPath: "inset(100%)",alpha: 0 })
+       .from("#gallery-7", {duration: 3,clipPath: "inset(0 50%)", color: "black" }, "-=8");
        return tl
 }
 
 // function galleryMotion(){
-//      var tl= gsap.timeline({scrollTrigger:{trigger:"#gallery-1", scrub:true, markers:true, end: "center 20%", start: "center 90%"}});
+//     var tl= gsap.timeline({scrollTrigger:{trigger:"#gallery-1", scrub:true, markers:true, end: "center 20%", start: "center 90%"}});
+//      tl.from("#gallery-1", {duration: 3, clipPath: "circle(100%)",alpha: 0 })
+//        .from("#gallery-5", {duration: 3, clipPath: "circle(100%)",alpha: 0 },"-=1.5")
+//        .from("#gallery-2", {duration: 3, clipPath: "circle(100%)",alpha: 0 },"-=1.5")
+//        .from("#gallery-4", {duration: 3, clipPath: "circle(100%)",alpha: 0 },"-=1.5")
+//        .from("#gallery-3", {duration: 3, clipPath: "circle(100%)",alpha: 0 },"-=1.5")
+//        .from("#gallery-6", {duration: 3, clipPath: "circle(100%)",alpha: 0 })
+//        .from("#gallery-7", {duration: 3, alpha: 1 }, "+=8");
+//        return tl
+// }
+
+// function galleryMotion(){
+//      var tl= gsap.timeline({scrollTrigger:{trigger:"#gallery-1", scrub:true, markers:true, end: "center 40%", start: "center bottom"}});
 //       tl.to("#gallery-1", {duration: 3, clipPath: "circle(100%)",alpha: 1 })
 //         .to("#gallery-5", {duration: 3, clipPath: "circle(100%)",alpha: 1 },"-=1.5")
 //         .to("#gallery-2", {duration: 3, clipPath: "circle(100%)",alpha: 1 },"-=1.5")
 //         .to("#gallery-4", {duration: 3, clipPath: "circle(100%)",alpha: 1 },"-=1.5")
 //         .to("#gallery-3", {duration: 3, clipPath: "circle(100%)",alpha: 1 },"-=1.5")
 //         .to("#gallery-6", {duration: 3, clipPath: "circle(100%)",alpha: 1 })
-//         .from("#gallery-7", {duration: 3, alpha: 0 }, "+=8");
+//         .from("#gallery-7", {duration: 3, alpha: 1 }, "+=8");
 //         return tl
 // }
 
