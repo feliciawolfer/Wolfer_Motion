21548875e6b4b19989fcdc171a801d1efa717bcc
FeliciaJW <102631983+FeliciaJW@users.noreply.github.com>
2022-11-02 23:10:24 -0400

    template

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,90 +1,35 @@
 import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { GSDevTools } from "gsap/GSDevTools";
 
-gsap.registerPlugin(ScrollTrigger);
 
-function heroMotion(){
-    var tl = gsap.timeline({scrollTrigger:{trigger:"#spaces", markers:false, scrub:true, end: "bottom 45%", start: "top 60%"}})
-        tl.from("#spaces", { duration:1, alpha:0, x:-200})
-        .from("#people", { duration:1,scale:2.5, color: "black"},"-=1")
-        .from("#subhead", {duration:1, alpha:0, y:-50},"-=1")
-        .from("#history-middle", {duration:1, alpha:0, y:-50})
+gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
+
+function simpleMotion(){
+
+    var tl = gsap.timeline
+    
     return tl;
 }
 
-function historytextMotion(){
-    var tl = gsap.timeline({scrollTrigger:{trigger:"#header-1", markers:false, scrub:true, end: "bottom 45%", start: "top 80%"}})
-        tl.from("#header-1", { duration:1, alpha:0, x:-200})
-        .from("#paragraph-1", { duration:1, alpha:0, x:200})
-        .from("#header-2", { duration:1, alpha:0, x:-200})
-        .from("#paragraph-2", { duration:1, alpha:0, x:200})
-        .from("#header-3", { duration:1, alpha:0, x:-200})
-        .from("#paragraph-3", { duration:1, alpha:0, x:200})
+function patternMotion(){
+
+    var tl = gsap.timeline();
+
+    tl.from(".odd",{duration:2, drawSVG:0, stagger:0.25, rotate:180, transformOrgin:"center"},"spin")
+        fromTo(".even",{drawSVG:"0%, 0%"},{duration:2, drawSVG:"0% -100%", stagger:0.25, rotate:180, transformOrgin:"center"},"spin");
     return tl;
 }
 
-function hero2Motion(){
-    var tl = gsap.timeline({scrollTrigger:{trigger:"#hero-2",scrub:true, markers:false, start:"top bottom", end:"center 60%"}});
-    tl.from("#hero-2", {duration: 2, alpha:0, clipPath: "inset(50% 0)"})
-    .from("#hero-2 h1", {duration: 2, scale:10, color:"pink"},"-=.85")
-    .from("#hero-2 h2", {duration: 2, scale:10, color:"pink"},"-=.95");
+function uiMotion(){
+
+    var tl = gsap.timeline
+    
     return tl;
 }
-
-function galleryMotion(){
-    var tl= gsap.timeline({scrollTrigger:{trigger:"#gallery-1", scrub:true, markers:true, end: "center center", start: "center bottom"}});
-     tl.from("#gallery-1", {duration: 3, clipPath: "inset(100%)",alpha: 0})
-       .from("#gallery-5", {duration: 3,clipPath: "inset(100%)",alpha: 0 },"-=1.5")
-       .from("#gallery-2", {duration: 3,clipPath: "inset(100%)",alpha: 0 },"-=1.5")
-       .from("#gallery-4", {duration: 3,clipPath: "inset(100%)",alpha: 0 },"-=1.5")
-       .from("#gallery-3", {duration: 3,clipPath: "inset(100%)",alpha: 0 },)
-       .from("#gallery-6", {duration: 3,clipPath: "inset(100%)",alpha: 0 })
-       .from("#gallery-7", {duration: 3,clipPath: "inset(0 50%)", color: "black" }, "-=8");
-       return tl
-}
-
-// function galleryMotion(){
-//     var tl= gsap.timeline({scrollTrigger:{trigger:"#gallery-1", scrub:true, markers:true, end: "center 20%", start: "center 90%"}});
-//      tl.from("#gallery-1", {duration: 3, clipPath: "circle(100%)",alpha: 0 })
-//        .from("#gallery-5", {duration: 3, clipPath: "circle(100%)",alpha: 0 },"-=1.5")
-//        .from("#gallery-2", {duration: 3, clipPath: "circle(100%)",alpha: 0 },"-=1.5")
-//        .from("#gallery-4", {duration: 3, clipPath: "circle(100%)",alpha: 0 },"-=1.5")
-//        .from("#gallery-3", {duration: 3, clipPath: "circle(100%)",alpha: 0 },"-=1.5")
-//        .from("#gallery-6", {duration: 3, clipPath: "circle(100%)",alpha: 0 })
-//        .from("#gallery-7", {duration: 3, alpha: 1 }, "+=8");
-//        return tl
-// }
-
-// function galleryMotion(){
-//      var tl= gsap.timeline({scrollTrigger:{trigger:"#gallery-1", scrub:true, markers:true, end: "center 40%", start: "center bottom"}});
-//       tl.to("#gallery-1", {duration: 3, clipPath: "circle(100%)",alpha: 1 })
-//         .to("#gallery-5", {duration: 3, clipPath: "circle(100%)",alpha: 1 },"-=1.5")
-//         .to("#gallery-2", {duration: 3, clipPath: "circle(100%)",alpha: 1 },"-=1.5")
-//         .to("#gallery-4", {duration: 3, clipPath: "circle(100%)",alpha: 1 },"-=1.5")
-//         .to("#gallery-3", {duration: 3, clipPath: "circle(100%)",alpha: 1 },"-=1.5")
-//         .to("#gallery-6", {duration: 3, clipPath: "circle(100%)",alpha: 1 })
-//         .from("#gallery-7", {duration: 3, alpha: 1 }, "+=8");
-//         return tl
-// }
 
 
 var mainTl = gsap.timeline();
-mainTl.add(heroMotion())
-.add(historytextMotion())
-.add(hero2Motion())
-.add(galleryMotion());
-
-let $nav = $('nav[data-nav]');
-let isVisible = false;
-
-$(".nav-btns").on("click", function(){
-    console.log("click");
-    if(isVisible === false){
-        $nav.show();
-        isVisible = true;
-    }
-    else{
-        $nav.hide();
-        isVisible = false;
-    }
-})
+mainTl.add(simpleMotion())
+.add(patternMotion())
+.add(uiMotion())
